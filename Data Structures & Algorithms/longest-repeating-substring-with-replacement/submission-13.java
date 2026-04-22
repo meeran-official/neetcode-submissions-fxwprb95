2e2d284212b3e324b18507ed5d38f342758928d2
Meeran <meeran.official@gmail.com>
2026-04-22 07:51:40 +0530

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.java
@@ -0,0 +1,95 @@
+/*
+
+    0   1   2   3   4   5   6   7   8   9   10  11
+    A   B   A   A   B   B   B   B   B   A   A   B   B
+        L
+                                                        R
+    
+    windowSize - highFreqChar <= k
+
+    L: 0
+    R: 0
+    
+    - while R is in bound:
+        - find the frequency of a character with maximum frequency
+        - find the replacable characters
+        - if replacable character <= K
+            - update maximum window length
+            - more R forward, expanding the window
+        - if replacable character > K
+            - move L forward, shrinking the window
+
+    
+
+    window length: R - L + 1
+
+    maximum frequency:
+    - store character frequency in map
+    - find max: map.get(char) vs self
+
+    replacable characters: window length - highest frequency of a character
+
+    maximum window length: R - L + 1 vs self
+
+    A   A   B   A   B   B   A
+        L
+                        R
+    
+*/
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l = 0;
+        int r = 0;
+        int N = s.length();
+        int maxFreq = 0;
+        Map<Character, Integer> charFreq = new HashMap<>();
+        int maxWindLen = 0;
+
+        while(r < N) {
+            int windowLen = r - l + 1;
+            charFreq.merge(s.charAt(r), 1, Integer::sum);
+            maxFreq = Math.max(maxFreq, charFreq.get(s.charAt(r)));
+            int replacableCharCount = windowLen - maxFreq;
+            if(replacableCharCount <= k) {
+                maxWindLen = Math.max(maxWindLen, windowLen);
+            } else {
+                charFreq.merge(s.charAt(l), -1, Integer::sum);
+                l++;
+            }
+            r++;
+        }
+
+        return maxWindLen;
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
