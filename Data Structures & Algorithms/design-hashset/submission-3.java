7312ca9b8ef962bc240d4e6ad67528b22e46b6e8
Meeran <meeran.official@gmail.com>
2026-07-29 08:13:46 +0530

    Add: design-hashset - submission-3

---
+++ Data Structures & Algorithms/design-hashset/submission-3.java
@@ -0,0 +1,25 @@
+class MyHashSet {
+    boolean[] bool = new boolean[(int) Math.pow(10, 6)];
+    public MyHashSet() {
+    }
+    
+    public void add(int key) {
+        bool[key] = true;
+    }
+    
+    public void remove(int key) {
+        bool[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return bool[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
